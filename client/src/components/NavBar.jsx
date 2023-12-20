61a10f0e59b5dbf2308aedcd2d2f0504ee0b79f1
Christian-Betances <chrisbet313@gmail.com>
2023-12-20 18:53:19 -0500

    Added night mode to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useEffect, useState} from 'react'
 import './NavBar.css'
 
 function NavBar(){
 
+    const[toggleNightMode, setNightMode] = useState(false);
+
+    function handleButtonClick () {
+
+        setNightMode((prev) => !prev)
+    }
+
+    const nightMode = toggleNightMode && 'nightmode';
+
     return (
-        <nav>
+        <nav className={nightMode}>
         <ul>
             <li id="Home"><Link to="/">Home</Link></li>
         </ul>
         <ul>
             <li id="Shop"><Link to="/Shop">Shop</Link></li>
         </ul>
+        <div className="nightModeButton">
+            <button onClick={handleButtonClick}>Night Mode</button>
+        </div>
         </nav>
       )
     }
